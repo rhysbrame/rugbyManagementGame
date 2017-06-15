68ba336ff8bf68926376f9d996dbc6de370a7bab
Rhys Brame <rhysbrame@gmail.com>
2017-06-15 12:22:28 +0100

    projet set up in visual studio

---
+++ RugbyManagementGameV1/RugbyManagementGameV1/Properties/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace RugbyManagementGameV1.Properties
+{
+    public class Player
+    {
+        private string player;
+
+        public string PlayerClassName
+        {
+            get
+            {
+                return player;
+            }
+        }
+
+        public int PlayerClassSkill { get; set; }
+
+        public Player(string playerParameterName, int playerParameterSkill)
+        {
+            this.player = playerParameterName;
+            this.PlayerClassSkill = playerParameterSkill;
+        }
+    }
+}
---
+++ RugbyManagementGameV1/RugbyTests/TestPlayer.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using RugbyManagementGameV1.Properties;
+
+namespace RugbyTests
+{
+    [TestFixture()]
+    public class TestPlayer
+    {
+        private Player IndividualPlayer {get;set;}
+
+        [SetUp] 
+        public void SetUp() {
+            IndividualPlayer = new Player("Darren", 5);
+        }
+
+        [Test()]
+        public void TestPlayerNameIsSet()
+        {
+            Assert.AreEqual("Darren", IndividualPlayer.PlayerClassName);
+        }
+
+		[Test()]
+		public void TestPlayerNameChange()
+		{
+            /*
+             * NO VALUE IN THIS TEST
+             * READONLY PROPERTIES DONT COMPILE SO CANT BE TESTED
+             * TRUST MICROSOFT
+             */
+            IndividualPlayer.PlayerClassName = "Rhys";
+			Assert.AreEqual("Rhys", IndividualPlayer.PlayerClassName);
+		}
+
+        [Test()]
+		public void TestPlayerSkillIsSet()
+		{
+			Assert.AreEqual("Darren", IndividualPlayer.PlayerClassName);
+		}
+    }
+}
