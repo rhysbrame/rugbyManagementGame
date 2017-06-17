bf910fd36289a416b7a7d602f3ebecf7a8984e95
Rhys Brame <rhysbrame@gmail.com>
2017-06-17 20:13:11 +0100

    added update route and tested with Insomnia

--- RugbyManager2017/RugbyManager2017/Controllers/PlayerController.cs
+++ RugbyManager2017/RugbyManager2017/Controllers/PlayerController.cs
@@ -17,7 +17,7 @@ namespace RugbyManager2017.Controllers
 
 			if (_context.Players.Count() == 0)
 			{
-				_context.Players.Add(new Player { PlayerClassName = "Ken Owens" });
+				_context.Players.Add(new Player { PlayerClassName = "Ken Owens", PlayerClassPosition = 2, PlayerClassSkill = 14 });
 				_context.SaveChanges();
 			}
 		}
@@ -51,6 +51,44 @@ namespace RugbyManager2017.Controllers
 			_context.SaveChanges();
 
 			return CreatedAtRoute("GetPlayer", new { id = item.Id }, item);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Update(long id, [FromBody] Player item)
+		{
+			if (item == null || item.Id != id)
+			{
+				return BadRequest();
+			}
+
+			var player = _context.Players.FirstOrDefault(t => t.Id == id);
+			if (player == null)
+			{
+				return NotFound();
+			}
+
+			player.IsComplete = item.IsComplete;
+			player.PlayerClassName = item.PlayerClassName;
+            player.PlayerClassPosition = item.PlayerClassPosition;
+            player.PlayerClassSkill = item.PlayerClassSkill;
+
+			_context.Players.Update(player);
+			_context.SaveChanges();
+			return new NoContentResult();
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(long id)
+		{
+			var player = _context.Players.First(t => t.Id == id);
+			if (player == null)
+			{
+				return NotFound();
+			}
+
+			_context.Players.Remove(player);
+			_context.SaveChanges();
+			return new NoContentResult();
 		}
 
 	}
