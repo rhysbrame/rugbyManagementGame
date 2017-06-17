0b10d2c4faeae0981f1852a329a34724bc93c85c
Rhys Brame <rhysbrame@gmail.com>
2017-06-17 11:11:16 +0100

    added a new welcome page route

--- RugbyManagementGameV1/RugbyAPI/Controllers/HomeController.cs
+++ RugbyManagementGameV1/RugbyAPI/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace RugbyAPI.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        {            
             var mvcName = typeof(Controller).Assembly.GetName();
             var isMono = Type.GetType("Mono.Runtime") != null;
 
@@ -19,5 +19,11 @@ namespace RugbyAPI.Controllers
 
             return View();
         }
+
+        public string Welcome()
+        {
+            return "Welcome page";
+        }
+
     }
 }
