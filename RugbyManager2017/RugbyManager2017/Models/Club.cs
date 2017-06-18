86fbb7b51a5ebf885177e3ec9974b83a16595a79
Rhys Brame <rhysbrame@gmail.com>
2017-06-18 13:45:21 +0100

    added methods froms previous project to Club Class

--- RugbyManager2017/RugbyManager2017/Models/Club.cs
+++ RugbyManager2017/RugbyManager2017/Models/Club.cs
@@ -1,11 +1,62 @@
 ﻿using System;
+
 namespace RugbyManager2017.Models
 {
     public class Club : Teams
     {
         public long Id { get; set; }
         public string ClubName { get; set; }
+        public Player[] teamSheet;
         //competition
+
+        public Club(string name)
+        {
+            this.ClubName = name;
+            this.teamSheet = new Player[23];
+        }
+
+		public int TeamSheetCount()
+		{
+			int count = 0;
+			foreach (var player in teamSheet)
+			{
+				if (player != null)
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+
+		public bool TeamSheetFull()
+		{
+			return this.TeamSheetCount() == this.teamSheet.Length;
+		}
+
+		public void AddPlayer(Player player)
+		{
+			if (this.TeamSheetFull())
+			{
+				return;
+			}
+			int teamSheetIndex = this.TeamSheetCount();
+			this.teamSheet[teamSheetIndex] = player;
+		}
+
+		public int CalculateTeamSkill()
+		{
+            int teamSkill = 0;
+
+            if (this.TeamSheetFull())
+            {
+                foreach (var player in teamSheet)
+                {
+                    teamSkill += player.PlayerSkill;
+                }
+
+            }
+            return teamSkill;
+		}
 
     }
 }
--- RugbyManager2017/RugbyManager2017/Models/Player.cs
+++ RugbyManager2017/RugbyManager2017/Models/Player.cs
@@ -9,6 +9,13 @@ namespace RugbyManager2017.Models
         public string PlayerName { get; set; }
         public int PlayerPosition { get; set; }
         public int PlayerSkill { get; set; }
+
+        public Player(string name, int position, int skill)
+        {
+            this.PlayerName = name;
+            this.PlayerPosition = position;
+            this.PlayerSkill = skill;
+        }
     }
 
 }
